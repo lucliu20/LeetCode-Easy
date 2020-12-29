80baf32e15123afec2acf9b53bed657d252e8378
lucliu20 <76449030+lucliu20@users.noreply.github.com>
2020-12-29 10:39:06 -0800

    Two approaches added.

--- 1-Two Sum-2nd.py
+++ 1-Two Sum-2nd.py
@@ -13,26 +13,33 @@
 # Input: nums = [3,3], target = 6
 # Output: [0,1]
 
-# nums, target = [2,7,11,15], 9
+nums, target = [2,7,11,15], 9
 # nums, target = [3,2,4], 6
-nums, target = [1,3,3], 6
+# nums, target = [1,3,3], 6
 
-# Approach #1: LeeCode solution #3
+
+# Approach #1
+# Two-pass with Hash
 # class Solution:
 #     def twoSum(self, nums: list(), target: int) -> list():
-#         mydict = dict()
+#         myd = {k: v for v, k in enumerate(nums)}
+#         # print(type(myd), myd, myd.keys(), type(myd.keys()))
 #         for i in range(len(nums)):
-#             com = target - nums[i]
-#             if com in mydict:
-#                 return [mydict[com], i]
-#             mydict[nums[i]] = i
-#         return -1
+#             if (target - nums[i]) in myd.keys():
+#                 if myd[target - nums[i]] != i:
+#                     return [i, myd[target - nums[i]]]
+# 
+# solution = Solution()
+# print(solution.twoSum(nums, target))
 
-# Runtime: 44 ms, faster than 91.07% of Python3 online submissions for Two Sum.
-# Memory Usage: 14.2 MB, less than 100.00% of Python3 online submissions for Two Sum.
+# Runtime: 40 ms, faster than 96.30% of Python3 online submissions for Two Sum.
+# Memory Usage: 14.7 MB, less than 33.34% of Python3 online submissions for Two Sum.
+
 
 # Approach #2
+# One-pass with Hash
 """
+Illustration:
     nums = [2,7,11,15]  target = 9
     
     compliment = { 9-2  = 7  : "0",
@@ -45,33 +52,20 @@ nums, target = [1,3,3], 6
 	nums[1] =7 in complement:
 			return [0,1]
 """
-# class Solution:
-#     def twoSum(self, nums: list(), target: int) -> list():
-#         mydict = dict()
-#         for i in range(len(nums)):
-#             com = target - nums[i]
-#             if nums[i] in mydict:
-#                 return [mydict[nums[i]], i]
-#             mydict[com] = i
-#         return -1
 
-# Runtime: 36 ms, faster than 99.42% of Python3 online submissions for Two Sum.
-# Memory Usage: 14.4 MB, less than 100.00% of Python3 online submissions for Two Sum.
-
-# Two-pass
 class Solution:
     def twoSum(self, nums: list(), target: int) -> list():
-        myd = {k: v for v, k in enumerate(nums)}
-        # print(type(myd), myd, myd.keys(), type(myd.keys()))
+        myd = {}
         for i in range(len(nums)):
-            if (target - nums[i]) in myd.keys():
-                if myd[target - nums[i]] != i:
-                    return [i, myd[target - nums[i]]]
+            if nums[i] in myd:
+                return [myd[nums[i]], i]
+            myd[target - nums[i]] = i
 
 solution = Solution()
 print(solution.twoSum(nums, target))
 
 
-# Runtime: 40 ms, faster than 96.30% of Python3 online submissions for Two Sum.
-# Memory Usage: 14.7 MB, less than 33.34% of Python3 online submissions for Two Sum.
+# Runtime: 32 ms, faster than 99.89% of Python3 online submissions for Two Sum.
+# Memory Usage: 14.5 MB, less than 40.73% of Python3 online submissions for Two Sum.
+
 
