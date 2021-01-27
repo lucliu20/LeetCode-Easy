4764e22a3b619ae3751b13ff6f08734c85d288e5
lucliu20 <76449030+lucliu20@users.noreply.github.com>
2021-01-27 14:19:42 -0800

    Update 119-Pascal's Triangle II.py

--- 119-Pascal's Triangle II.py
+++ 119-Pascal's Triangle II.py
@@ -21,16 +21,44 @@ rowIndex = 3
 
 # recurrence relation: use the previous row array element values to calculate the current row array element values;
 # append [1] at either boarders of the row.
+# class Solution:
+#     def getRow(self, rowIndex: int) -> list():
+#         if rowIndex == 0:
+#             return [1]
+#         lastRow = self.getRow(rowIndex-1)
+#         res = [1]
+#         for i in range(len(lastRow)-1):
+#             res.append(lastRow[i]+lastRow[i+1])
+#         res.append(1)
+#         return res
+
+
+# Runtime: 32 ms, faster than 60.64% of Python3 online submissions for Pascal's Triangle II.
+# Memory Usage: 14.3 MB, less than 48.07% of Python3 online submissions for Pascal's Triangle II.
+
+
+# Iterate way
 class Solution:
     def getRow(self, rowIndex: int) -> list():
-        if rowIndex == 0:
-            return [1]
-        lastRow = self.getRow(rowIndex-1)
         res = [1]
-        for i in range(len(lastRow)-1):
-            res.append(lastRow[i]+lastRow[i+1])
-        res.append(1)
+        if rowIndex == 0: return res
+        for n in range(1, rowIndex+1):
+            t = [1]
+            for i in range(n):
+                if i == n-1:
+                    t.append(1)
+                else:
+                    t.append(res[i] + res[(i+1)%n])
+            res.clear()
+            res = [k for k in t]
+
         return res
+
+# Runtime: 32 ms, faster than 64.88% of Python3 online submissions for Pascal's Triangle II.
+# Memory Usage: 14.3 MB, less than 24.75% of Python3 online submissions for Pascal's Triangle II.
+
+# class Solution:
+#     def getRow(self, rowIndex: int) -> list():
         # My initial recursive way: "Time Limit Exceeded"
         # def get(row, col):
         #     if row == col or col == 0:
@@ -44,6 +72,4 @@ class Solution:
 solution = Solution()
 print(solution.getRow(rowIndex))
 
-# Runtime: 32 ms, faster than 60.64% of Python3 online submissions for Pascal's Triangle II.
-# Memory Usage: 14.3 MB, less than 48.07% of Python3 online submissions for Pascal's Triangle II.
 
