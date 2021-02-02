b7b3deb6cb12da9088b2246333a88d24a2d4aac4
lucliu20 <76449030+lucliu20@users.noreply.github.com>
2021-02-02 11:53:18 -0800

    Update 104-Maximum Depth of Binary Tree.py

--- 104-Maximum Depth of Binary Tree.py
+++ 104-Maximum Depth of Binary Tree.py
@@ -38,11 +38,12 @@ root.right.right = TreeNode(7)
 
 # root = TreeNode(0)
 
-class Solution:
-    def maxDepth(self, root: TreeNode) -> int:
-        if root is None:
-            return 0
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+# Approach when I was doing the Recursive Explore Card
+# class Solution:
+#     def maxDepth(self, root: TreeNode) -> int:
+#         if root is None:
+#             return 0
+#         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
         # My initial way:
         # def depth(node, dep):
@@ -53,9 +54,41 @@ class Solution:
         #     return max(hl, hr)
         # return depth(root, 0)
 
+# Runtime: 40 ms, faster than 73.16% of Python3 online submissions for Maximum Depth of Binary Tree.
+# Memory Usage: 16.5 MB, less than 10.17% of Python3 online submissions for Maximum Depth of Binary Tree.
+
+
+# Approaches when I was doing the Binary Tree Explore Card on 02/02/2021
+# Bottom up
+# class Solution:
+#     def maxDepth(self, root: TreeNode) -> int:
+#         if not root: return 0
+#         left = self.maxDepth(root.left)
+#         right = self.maxDepth(root.right)
+#         return max(left, right) + 1
+
+# Runtime: 56 ms, faster than 10.82% of Python3 online submissions for Maximum Depth of Binary Tree.
+# Memory Usage: 16.1 MB, less than 38.83% of Python3 online submissions for Maximum Depth of Binary Tree.
+
+
+# Top down
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        self.res = 0
+        def helper(node, depth):
+            if not node: return
+            if node.left is None and node.right is None:
+                self.res = max(self.res, depth)
+            helper(node.left, depth+1)
+            helper(node.right, depth+1)
+        
+        helper(root, 0)
+        return self.res+1
+
+# Runtime: 36 ms
+# Memory Usage: 16.5 MB
+
 solution = Solution()
 print(solution.maxDepth(root))
 
-# Runtime: 40 ms, faster than 73.16% of Python3 online submissions for Maximum Depth of Binary Tree.
-# Memory Usage: 16.5 MB, less than 10.17% of Python3 online submissions for Maximum Depth of Binary Tree.
 
