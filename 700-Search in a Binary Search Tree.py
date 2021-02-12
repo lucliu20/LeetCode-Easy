c801b5d7f565834de8c6c0839e1c83d3b70b5602
lucliu20 <76449030+lucliu20@users.noreply.github.com>
2021-02-12 09:08:27 -0800

    Update 700-Search in a Binary Search Tree.py

--- 700-Search in a Binary Search Tree.py
+++ 700-Search in a Binary Search Tree.py
@@ -34,21 +34,47 @@ root.right = TreeNode(7)
 root.left.left = TreeNode(1)
 root.left.right = TreeNode(3)
 
-class Solution:
-    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
-        if not root:
-            return root
-        if root.val == val:
-            return root
-        if root.val > val:
-            return self.searchBST(root.left, val)
-        else:
-            return self.searchBST(root.right, val)
-
-
-solution = Solution()
-solution.searchBST(root, val)
+# Recursively
+# DFS
+# Top-down
+# class Solution:
+#     def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+#         if not root:
+#             return root
+#         if root.val == val:
+#             return root
+#         if root.val > val:
+#             return self.searchBST(root.left, val)
+#         else:
+#             return self.searchBST(root.right, val)
 
 # Runtime: 80 ms
 # Memory Usage: 16.1 MB
+
+
+# Iteratively
+# DFS
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root: return None
+        curr = root
+        stack = []
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            if curr.val == val:
+                return curr
+            curr = curr.right
+        return None
+
+# Runtime: 68 ms, faster than 91.12% of Python3 online submissions for Search in a Binary Search Tree.
+# Memory Usage: 16 MB, less than 98.07% of Python3 online submissions for Search in a Binary Search Tree.
+
+
+solution = Solution()
+print(solution.searchBST(root, val))
+
+
 
